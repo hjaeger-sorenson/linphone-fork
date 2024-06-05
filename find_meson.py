fe579a7c191962dbdc9e9c8e5dc6dd51c494f9be
Hans Jaeger <hjaeger@sorenson.com>
2024-06-05 13:21:23 -0400

    updating test

--- find_meson.py
+++ find_meson.py
@@ -1,6 +1,6 @@
 import os
 
-for dirpath, dnames, fnames in os.walk("C:\\"):
+for dirpath, dnames, fnames in os.walk("C:\\hostedtoolcache\\windows\\Python"):
     for f in fnames:
         if(f == "meson.exe"):
             print(os.path.join(dirpath, f))
